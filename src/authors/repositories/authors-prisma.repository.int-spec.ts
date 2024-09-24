58251aa8bb5bfd167acada5f1433f0944842c8db
virgilio-vpr <virgilio.vpr@gmail.com>
2024-09-23 21:28:41 -0300

    Implementig updating method and test author

--- src/authors/repositories/authors-prisma.repository.int-spec.ts
+++ src/authors/repositories/authors-prisma.repository.int-spec.ts
@@ -54,6 +54,33 @@ describe('AuthorsPrismaRepository Integration Tests', () => {
     expect(author).toMatchObject(data)
   })
 
+  test('should throws an error when updating a author not found', async () => {
+    const data = AuthorDataBuilder({})
+    const author = {
+      id: '796c5a25-1d3b-4228-9a75-06f416c6e218',
+      ...data,
+    }
+    await expect(repository.update(author)).rejects.toThrow(
+      new NotFoundError(
+        'Author not found using ID 796c5a25-1d3b-4228-9a75-06f416c6e218',
+      ),
+    )
+  })
+
+  test('should update a author', async () => {
+    const data = AuthorDataBuilder({})
+    const author = await prisma.author.create({
+      data,
+    })
+
+    const result = await repository.update({
+      ...author,
+      name: 'new name',
+    })
+
+    expect(result.name).toBe('new name')
+  })
+
   describe('search method', () => {
     test('should only apply pagination when the parameters are null', async () => {
       const createdAt = new Date()
@@ -76,6 +103,82 @@ describe('AuthorsPrismaRepository Integration Tests', () => {
       result.items.forEach(item => {
         expect(item.id).toBeDefined()
       })
+
+      result.items.reverse().forEach((item, index) => {
+        expect(`${item.email}${index + 1}@a.com`)
+      })
+    })
+
+    test('should apply pagination and ordering', async () => {
+      const createdAt = new Date()
+      const data = []
+      const arrange = 'badec'
+      arrange.split('').forEach((element, index) => {
+        const timestamp = createdAt.getTime() + index
+        data.push({
+          ...AuthorDataBuilder({ name: element }),
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const result1 = await repository.search({
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+
+      expect(result1.items[0]).toMatchObject(data[1])
+      expect(result1.items[1]).toMatchObject(data[0])
+
+      const result2 = await repository.search({
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+
+      expect(result2.items[0]).toMatchObject(data[4])
+      expect(result2.items[1]).toMatchObject(data[2])
+    })
+
+    test('should apply pagination, filter and ordering', async () => {
+      const createdAt = new Date()
+      const data = []
+      const arrange = ['test', 'a', 'TEST', 'b', 'Test']
+      arrange.forEach((element, index) => {
+        const timestamp = createdAt.getTime() + index
+        data.push({
+          ...AuthorDataBuilder({ name: element }),
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const result1 = await repository.search({
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      })
+
+      expect(result1.items[0]).toMatchObject(data[0])
+      expect(result1.items[1]).toMatchObject(data[4])
+
+      const result2 = await repository.search({
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      })
+
+      expect(result2.items[0]).toMatchObject(data[2])
+      expect(result2.items.length).toBe(1)
     })
   })
 })
